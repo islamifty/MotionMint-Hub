59e15b8b9f2a71b429480d26c41a54634e175f0d
Islam Ifty <islamifty11@gmail.com>
2025-08-28 06:12:07 +0000

    Local db.json not found. Creating from template.Login failed: User not f

---
+++ src/next.config.ts
@@ -0,0 +1,26 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  compress: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+   experimental: {
+    serverComponentsExternalPackages: ['redis'],
+  },
+};
+
+export default nextConfig;
